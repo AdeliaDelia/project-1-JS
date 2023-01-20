2beff6baea1d014b73beb71cbae16963a944c8e9
Adelina Kalnitskaya <delya.kalniska@gmail.com>
2023-01-20 21:17:27 +0300

    modified method writeYourGenres

--- js/script.js
+++ js/script.js
@@ -46,11 +46,29 @@ const personalMovieDB = {
             console.log(personalMovieDB);
         }
     },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.private) {
+            personalMovieDB.private = false;
+        } else {
+            personalMovieDB.private = true;
+        }
+    },
     // функция записывает в массив данных genres ответы пользователя
     writeYourGenres: function () {
         for (let i = 1; i <= 3; i++) {
-            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+            if (genre === '' || genre === null) {
+                console.log('Вы неверно ввели данные или ничего не ввели');
+                i--;
+            } else {
+                personalMovieDB.genres [i -1]= genre;
+            }
         }
+
+        personalMovieDB.genres.forEach((item, i) => {
+            console.log(`Любимый жанр ${i + 1} - это ${item}`);
+        });
     }
 
 };
